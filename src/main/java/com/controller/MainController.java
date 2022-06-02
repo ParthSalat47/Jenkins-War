6b0f61851198832aa2c986d7679e89c1a5a2deb7
Parth Salat <parth.sict18@sot.pdpu.ac.in>
2022-06-02 13:45:09 +0530

    /home working perfectly

---
+++ src/main/java/com/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/home")
+	public String homePage()
+	{
+		return "home";
+	}
+	
+}
